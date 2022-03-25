40c6a87f10750c032b7ca2faa3a679484ce5010b
CTimet <92621765+CTimet@users.noreply.github.com>
2022-03-25 21:30:21 +0800

    Update BektMain.java

--- src/main/java/io/github/ctimet/bekt/BektMain.java
+++ src/main/java/io/github/ctimet/bekt/BektMain.java
@@ -7,6 +7,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 /** Main Class
  * @author CTimet
+ * @author CLIEDS
  * @version beta1.0.0
  */
 public class BektMain extends JavaPlugin implements SlimefunAddon
